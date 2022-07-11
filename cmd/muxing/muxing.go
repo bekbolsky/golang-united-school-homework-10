b29377268aaf497d20a260a6fe9a3ee4d64bdcea
Bekbolat Kuralbayev <bkb@live.ru>
2022-07-12 02:08:46 +0600

    some code refining

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -11,6 +11,58 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// CatchAllHandler /** Handles all requests.
+// If request is not handled by any other handler,
+// then this handler will be called.
+// It will return 200 status code.
+func CatchAllHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+// BadStatusHandler handles "/bad" endpoint.
+// It will return 500 status code.
+func BadStatusHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+// NameHandler handles "/name/{PARAM}" endpoint.
+// It writes "Hello, {PARAM}!" to response.
+// It returns 200 status code.
+func NameHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	param := vars["PARAM"]
+
+	fmt.Fprintf(w, "Hello, %s!", param)
+}
+
+// HeaderHandler handles "/headers" endpoint.
+// It receives two headers: a and b.
+// It adds a+b to the response header.
+// a+b is the sum of a and b.
+func HeaderHandler(w http.ResponseWriter, r *http.Request) {
+	a := r.Header.Get("a")
+	b := r.Header.Get("b")
+	aValue, _ := strconv.Atoi(a)
+	bValue, _ := strconv.Atoi(b)
+
+	sum := aValue + bValue
+	w.Header().Add("a+b", strconv.Itoa(sum))
+	// w.Header().Set("a+b", strconv.Itoa(sum))
+
+}
+
+// DataHandler handles "/data" endpoint.
+// It receives body of the request and writes it to response.
+// It writes "I got message: {BODY}" to response.
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, "I got message:\n%s", string(body))
+}
+
 /**
 Please note Start functions is a placeholder for you to start your own solution.
 Feel free to drop gorilla.mux if you want and use any other solution available.
@@ -42,42 +94,4 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
-}
-
-func CatchAllHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
-
-func BadStatusHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
-
-}
-
-func NameHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	param := vars["PARAM"]
-
-	fmt.Fprintf(w, "Hello, %s!", param)
-}
-
-func HeaderHandler(w http.ResponseWriter, r *http.Request) {
-	a := r.Header.Get("a")
-	b := r.Header.Get("b")
-	aValue, _ := strconv.Atoi(a)
-	bValue, _ := strconv.Atoi(b)
-
-	sum := aValue + bValue
-	w.Header().Add("a+b", strconv.Itoa(sum))
-	// w.Header().Set("a+b", strconv.Itoa(sum))
-
-}
-
-func DataHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Fprintf(w, "I got message:\n%s", string(body))
 }
